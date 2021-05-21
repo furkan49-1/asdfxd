28fc12887651208b3d06f682ac0360b17cd4481a
furkan49-1 <84560360+furkan49-1@users.noreply.github.com>
2021-05-21 09:49:59 +0300

    sa

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1621577224,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/tile-sheet0.png",
+		"images/tiledbackground3.png",
+		"images/tiledbackground4.png",
+		"images/walk-sheet0.png",
+		"images/player-sheet0.png",
+		"images/clipart-sheet0.png",
+		"images/tiledbackground5.png",
+		"images/monster-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/tiledbackground8.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
